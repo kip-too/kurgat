d2b76e56e0d904357442b7e0d33ec193e03070d8
kip-too <hoseakiptoo201@gmail.com>
2024-04-16 13:33:27 +0300

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                     Kurgat
                 </Link>
             </div>
-            <a href="https://drive.google.com/file/d/1iIiTFAKQY22cn-xIH38sOZsBYWSRIAj4/view?usp=sharing" className="cta-btn">Résumé</a>
+            <a href="https://drive.google.com/file/d/1-f4nbN8HgE7L-Uz30-kb1qNmWOE6FCEJ/view?usp=sharing" className="cta-btn">Résumé</a>
         </div>
     )
 }
